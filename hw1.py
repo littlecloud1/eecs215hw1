2f9b600e8ef3cf99d59efe08b1b9d193b1cfdb3d
Lai Man Tang <cloudtang030@gmail.com>
2018-01-31 19:55:37 -0800

    add comments

--- hw1.py
+++ hw1.py
@@ -1,7 +1,17 @@
+"""
+Hw1 bouns ex1: Graph dataset generator
+Output the #vectice, #edges and nodes pair:actor+actor.
+
+1/23/2018
+
+lai man tang
+yuan qin
+
+"""
+
 import sys
-actor_dict = {}
-movie_dict = {}
-#node = []
+actor_dict = {} #dictionary for actor: key: actor; value: list of actors
+movie_dict = {} #dictionary for movie: key: move; value: list of actors
 
 
 
@@ -13,20 +23,18 @@ def readfile(filename):
 			actor = actor_in_movie[0]
 			movie = actor_in_movie[1]
 			
-			#if actor not in node:
-			#	node.append(actor)
+			#add data in movie dict
 			if movie not in movie_dict:
 				list = []
-				#list.append(node.index(actor))
 				list.append(actor)
 				movie_dict[movie] = list
 			else:
-				#movie_dict[movie].append(node.index(actor))
 				movie_dict[movie].append(actor)
 				
 		
 	f.close()
-	
+
+#build actor dictionary
 def builddict():	
 	for movie in movie_dict:
 		for i in range(0, len(movie_dict[movie])-1):
@@ -37,18 +45,20 @@ def builddict():
 			
 			
 			
-			
+#output function 			
 def output(outfile):
 	
 	edge = []
-	visited = []
+	visited = [] #keep visited actor list
 	for key1 in actor_dict:
 		for key2 in actor_dict[key1]:
+		    # if key1-key2 pair not added, build the edge
 			if key2 not in visited:
 				s =  str(key1)+ '+'+str(key2)
 				edge.append(s)
 		visited.append(key1)
 	
+	#file output
 	f = open(outfile,'w')
 	f.write(str(len(actor_dict))+"\n")
 	f.write(str(len(edge))+"\n")
@@ -60,7 +70,7 @@ def output(outfile):
 	
 	
 
-	
+# check two actor's dictionary, if no redunancy, add to actor dictionary
 def addtodict(key1, key2):
 	if key1 == key2 :
 		return
